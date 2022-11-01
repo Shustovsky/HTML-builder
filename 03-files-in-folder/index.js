897d58192d02420bd5ca5c428ad71dbcdfdaeb79
A. Shustovsky <a.shustovsky@gmail.com>
2022-11-01 18:20:10 +0400

    feat: task 03-files-in-folder is done +-

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+const secretPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(secretPath, (err, files) => {
+  if (err) throw err;
+
+  files.forEach(file => {
+    const eachPath = path.join(secretPath, file)
+    const name = path.parse(eachPath).name;
+    const ext = path.extname(eachPath);
+
+    fs.stat(eachPath, (err, stat) => {
+      if (err) console.log(err);
+
+      const sizeBite = stat.size;
+      if (sizeBite) {
+        console.log(chalk.green(`${name} - ${ext} - ${sizeBite} bytes`));
+      };
+    });
+  });
+});

56737d528f13ea2c6de2ebcd17ababff717a3104
A. Shustovsky <a.shustovsky@gmail.com>
2022-11-02 14:19:39 +0400

    feat: task completed!!! ☺️

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 
+copyDir = () => {
+  const pathToOriginal = path.join(__dirname, 'files');
+  const pathToCopy = path.join(__dirname, 'files-copy');
+
+  fs.access(pathToCopy, error => {
+    if (error) {
+      createAndCopyFolder();
+      console.log(chalk.bgGreen('Folder created!'));
+    } else {
+      recopyFiles();
+      console.log(chalk.bgRed('Warning! The folder has already been created, files will be overwritten!'));
+    }
+  });
+
+  function createAndCopyFolder() {
+    fs.mkdir(pathToCopy, { recursive: true }, err => {
+      if (err) throw err;
+      copyFiles();
+    });
+  };
+
+  function recopyFiles() {
+    fs.rm(pathToCopy, { recursive: true, force: true }, (err) => {
+      if (err) throw err;
+      createAndCopyFolder();
+    });
+  };
+
+  function copyFiles() {
+    fs.readdir(pathToOriginal, (err, files) => {
+      if (err) throw err;
+
+      files.forEach(file => {
+        const fileOriginal = path.join(pathToOriginal, file);
+        const fileCopy = path.join(pathToCopy, file);
+
+        fs.copyFile(fileOriginal, fileCopy, err => {
+          if (err) throw err;
+          console.log(chalk.green(`${file} - Copy completed.`));
+        });
+      });
+    });
+  };
+
+};
+copyDir();
+
+
+
+
+
+// fs.readdir(pathToCopy, (err, files) => {
+//   if (err) throw err;
+
+//   files.forEach(file => {
+//     const fileCopy = path.join(pathToCopy, file);
+
+//     fs.unlink(fileCopy, err => {
+//       if (err) throw err;
+
+//     });
+//   });
+
+//   copyFiles();
+// });

0a46847eeef1fccb5a32ff86f05c06ceff66a3ad
A. Shustovsky <a.shustovsky@gmail.com>
2022-11-04 17:36:55 +0400

    fix: add correct logs in task 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,43 +1,43 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 
 const styles = path.join(__dirname, 'styles');
-const bundle = path.join(__dirname, 'project-dist', 'bundle.css')
+const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
 function createBundle() {
   fs.writeFile(bundle, '', err => {
     if (err) throw err;
   });
-  console.log(`Bundle created`);
+  console.log(chalk.green(`Bundle created.`));
 };
 createBundle();
 
 function searchCSS() {
   fs.readdir(styles, (err, files) => {
     if (err) throw err;
-
     files.forEach(file => {
       const pathFile = path.join(styles, file);
       if (path.extname(pathFile) === '.css') {
-        console.log(`--CSS file founded ${file}`);
-        readAndFill(pathFile);
-      }
+        console.log(chalk.yellow(`--CSS file founded ${file}`));
+        readAndFill(pathFile, file);
+      };
     });
   });
 };
 searchCSS();
 
-function readAndFill(file) {
+function readAndFill(file, name) {
   fs.readFile(file, 'utf-8', (err, data) => {
     if (err) throw err;
-    console.log(`----Read file ${file}`)
-    fillBundle(data);
+    console.log(chalk.cyan(`----Read file ${name}`));
+    fillBundle(data, name);
   });
 };
 
-function fillBundle(data) {
+function fillBundle(data, name) {
   fs.appendFile(bundle, `${data}\n`, err => {
     if (err) throw err;
-    // console.log(data);
+    console.log(chalk.green(`------Record file ${name}`));
   });
 };
